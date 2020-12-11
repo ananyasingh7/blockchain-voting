b4d0c4d0e1eb6cd6c64b2015fed4206276d65a63
Ananya Singh <as2863@njit.edu>
2020-12-11 14:46:39 -0500

    started work on the frontend

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.use(express.static('public/register.html'))
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
 })
+
+
