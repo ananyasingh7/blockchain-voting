b674d1a8b871118b205a83bc3fa162534b74a6c8
Ananya Singh <as2863@njit.edu>
2020-12-13 02:18:39 -0500

    fixed dependencies

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-app.use(express.static('public/register.html'))
+app.use(express.static('web-app/public/'));
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`)
